b901445b551db7217b3a7cfeae11f98f0dff933e
Huan Du <i@huandu.me>
2021-02-24 09:54:53 +0800

    add copyright declaration

--- clone_sample_test.go
+++ clone_sample_test.go
@@ -1,3 +1,6 @@
+// Copyright 2019 Huan Du. All rights reserved.
+// Licensed under the MIT license that can be found in the LICENSE file.
+
 package clone
 
 import "fmt"
--- wrapper.go
+++ wrapper.go
@@ -1,8 +1,7 @@
+// Copyright 2019 Huan Du. All rights reserved.
 // Licensed under the MIT license that can be found in the LICENSE file.
 
 package clone
-
-// Copyright 2019 Huan Du. All rights reserved.
 
 import (
 	"encoding/binary"
